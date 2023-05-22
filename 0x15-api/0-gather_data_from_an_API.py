c2ebd30b50967e5faf689c7f746ccc102ec65001
NGOIMBU <imbubeauty80@gmail.com>
2023-05-22 23:35:11 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,19 +11,21 @@ if __name__ == '__main__':
     url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
-    employeeName = 'Kingsley'
+    employeeName = "Janet"
+
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
-    done = ('5/6')
-    done_tasks = [4]
+    done = 0
+    done_tasks = []
 
     for task in tasks:
         if task.get('completed'):
             done_tasks.append(task)
-            #done += 1
+            done += 1
 
-    print(employeeName, 'is done with tasks', '(',done,')')
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
